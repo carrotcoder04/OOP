e879fd4b3982f2db33d0f018bd48946fe33f6fd9
carrotcoder04 <135999189+carrotcoder04@users.noreply.github.com>
2024-11-03 15:34:52 +0700

    big integer

--- Main.java
+++ Main.java
@@ -1,100 +1,33 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
-     public static void main(String[] args)  {
-          ClassManager classManager = new ClassManager();
-          Scanner scanner = new Scanner(System.in);
-          int n = scanner.nextInt();
-          int m = scanner.nextInt();
-          while(n-- > 0) {
-               BaseClass firstClass = classManager.getClass(scanner.next());
-               String relationship = scanner.next();
-               if(relationship.equals("is-a")) {
-                    BaseClass secondClass = classManager.getClass(scanner.next());
-                    firstClass.setParent(secondClass);
+     public static void main(String[] args) throws FileNotFoundException {
+          Scanner sc = new Scanner(new File("DAYSO.in"));
+          int n = sc.nextInt();
+          BigInteger min = null;
+          BigInteger max = null;
+          BigInteger sum = BigInteger.ZERO;
+          while (n-- > 0) {
+               BigInteger number = sc.nextBigInteger();
+               if(min == null) {
+                    min = number;
+                    max = number;
                }
-               else if(relationship.equals("has-a")) {
-                    BaseClass property = classManager.getClass(scanner.next());
-                    firstClass.addProperty(property);
-               }
-          }
-          for(int i=1;i<=m;i++) {
-               BaseClass firstClass = classManager.getClass(scanner.next());
-               String relationship = scanner.next();
-               if(relationship.equals("is-a")) {
-                    BaseClass secondClass = classManager.getClass(scanner.next());
-                    if(firstClass.isA(secondClass)) {
-                         System.out.println("Query " + i + ": true");
+               else {
+                    if(number.compareTo(min) < 0) {
+                         min = number;
                     }
-                    else {
-                         System.out.println("Query " + i + ": false");
-
+                    if(number.compareTo(max) > 0) {
+                         max = number;
                     }
                }
-               else if(relationship.equals("has-a")) {
-                    BaseClass property = classManager.getClass(scanner.next());
-                    if(firstClass.hasA(property)) {
-                         System.out.println("Query " + i + ": true");
-                    }
-                    else {
-                         System.out.println("Query " + i + ": false");
-                    }
-               }
+               sum = sum.add(number);
           }
-     }
-}
-class ClassManager {
-     private final HashMap<String,BaseClass> classMap;
-     public ClassManager() {
-          classMap = new HashMap<>();
-     }
-     public BaseClass getClass(String className) {
-          if(!classMap.containsKey(className)) {
-               BaseClass newClass = new BaseClass(className);
-               classMap.put(className,newClass);
-               return newClass;
-          }
-          return classMap.get(className);
-     }
-}
-class BaseClass {
-     private final String name;
-     private BaseClass parent;
-     private final ArrayList<BaseClass> properties;
-     public BaseClass(String name) {
-          this.name = name;
-          properties = new ArrayList<>();
-     }
-     public boolean isA(BaseClass baseClass) {
-          if(this == baseClass) {
-               return true;
-          }
-          if (parent != null) {
-               return parent.isA(baseClass);
-          }
-          return false;
-     }
-     public void setParent(BaseClass baseClass) {
-          parent = baseClass;
-     }
-     public boolean hasA(BaseClass property) {
-          for(BaseClass p : properties) {
-               if(p.isA(property)) {
-                    return true;
-               }
-          }
-          if(parent != null) {
-               return parent.hasA(property);
-          }
-          else {
-               return false;
-          }
-     }
-     public void addProperty(BaseClass property) {
-          this.properties.add(property);
+          System.out.println(min);
+          System.out.println(max);
+          System.out.println(sum);
      }
 }
